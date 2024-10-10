0bce10843c293aa2408e3fedb745f095fb547bcd
Anders Bilfeldt <abilfeldt@gmail.com>
2024-10-10 13:30:42 +0200

    Remove build

--- smart-send-logistics/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-plugins'), 'version' => '611de3490800bd5a57db');
--- smart-send-logistics/build/smart-send-block-frontend.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wc-blocks-checkout', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7819546ed87f2556c66f');
